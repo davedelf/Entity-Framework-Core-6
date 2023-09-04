387b336077fd684957094ce0e1996da2e3ca440b
111858-DelfinoDavidElías <davideliasdelfino@gmail.com>
2023-09-04 15:54:36 -0300

    Consultando datos
    Obteniendo el primer registro con First y FirstOrDefault

--- Controllers/GeneroController.cs
+++ Controllers/GeneroController.cs
@@ -34,5 +34,43 @@ namespace EFCorePeliculas.Controllers
             
         }
 
+        /*First y FirstOrDefault*/
+
+        /*First trae el primer registro que cumpla con la condición, pero si no encuentra nada entonces arroja Error 500
+         FirstOrDefault trae el primer registro que cumpla con la condición y si no encuentra nada entonces trae null*/
+
+        [HttpGet("primer")]
+        public async Task<ActionResult<Genero>> Primer()
+        {
+            var genero = await _context.Generos.FirstOrDefaultAsync(g => g.Nombre.StartsWith("C"));
+
+            if(genero == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return genero;
+            }
+
+
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Genero>> Get(int id)
+        {
+            var genero=await _context.Generos.FirstOrDefaultAsync(g=>g.Id==id);
+
+            if(genero == null)
+            {
+                return NotFound();
+
+            }
+            else
+            {
+                return genero;
+            }
+        }
+
     }
 }
